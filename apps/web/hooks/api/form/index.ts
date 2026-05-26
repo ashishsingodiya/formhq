ee56ef06021f9fffd57a2f4b48c953324614ada0
Ashish Singodiya <ashishsingodiya1@gmail.com>
2026-05-27 00:20:00 +0530

    feat(form): add useCreateForm hook for form creation mutation

---
+++ apps/web/hooks/api/form/index.ts
@@ -0,0 +1,25 @@
+import { trpc } from "~/trpc/client";
+
+export const useCreateForm = () => {
+  const {
+    mutateAsync: createFormAsync,
+    mutate: createForm,
+    error,
+    failureCount,
+    isError,
+    isIdle,
+    isSuccess,
+    status,
+  } = trpc.form.createForm.useMutation();
+
+  return {
+    createFormAsync,
+    createForm,
+    error,
+    failureCount,
+    isError,
+    isIdle,
+    isSuccess,
+    status,
+  };
+};
