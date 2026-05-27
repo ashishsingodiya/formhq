dd392c03cb5b3a20b1c7d9f09855a7b1ee51c354
Ashish Singodiya <ashishsingodiya1@gmail.com>
2026-05-27 05:46:40 +0530

    feat(form-submissions): add form_submissions table and model with foreign key to forms

---
+++ packages/database/models/form-submissions.ts
@@ -0,0 +1,25 @@
+import { index, json, pgTable, timestamp, uuid } from "drizzle-orm/pg-core";
+import { formsTable } from "./form";
+
+export interface FormSubmissionValue {
+  formFieldId: string;
+  value: string;
+}
+
+export type FormSubmissionValueRow = FormSubmissionValue[];
+
+export const formSubmissionTable = pgTable(
+  "form_submissions",
+  {
+    id: uuid("id").primaryKey().defaultRandom(),
+
+    formId: uuid("form_id")
+      .references(() => formsTable.id)
+      .notNull(),
+
+    values: json("values").$type<FormSubmissionValueRow>(),
+
+    createdAt: timestamp("created_at").defaultNow(),
+  },
+  (table) => [index("form_submissions_form_id_idx").on(table.formId)],
+);
